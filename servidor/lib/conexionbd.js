8b458c4f2694d86a299c21404b4c78834df59405
Marco Luchi <marcoaluchi@hotmail.com>
2020-04-06 11:41:16 -0300

    se agrega dependencia y conexion a bbdd

--- servidor/lib/conexionbd.js
+++ servidor/lib/conexionbd.js
@@ -1,11 +1,11 @@
 var mysql      = require('mysql');
 
 var connection = mysql.createConnection({
-  host     : 
-  port     : 
-  user     : 
-  password : 
-  database : 
+  host: 'localhost',
+  port: '3306',
+  user: 'root', 
+  password: 'eduardo11',
+  database : 'proyecto',
 });
 
 module.exports = connection;
