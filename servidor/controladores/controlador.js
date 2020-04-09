c99ffa6c14365ab2dabeff19c7ecfcb0e0289ccb
Marco Luchi <marcoaluchi@hotmail.com>
2020-04-09 16:13:40 -0300

    se agrego funcionalidad clic a pelicula y descripcion

--- servidor/controladores/controlador.js
+++ servidor/controladores/controlador.js
@@ -2,47 +2,48 @@ var con = require('../lib/conexionbd')
 function devolverTodasLasPeliculas(req,res){
     var sql;
     var total;
-    var pagina = req.query.pagina;
     var cantidad = req.query.cantidad;
+	var pagina = (req.query.pagina - 1) * cantidad;
     var anio = req.query.anio;
     var titulo = req.query.titulo;
     var genero = req.query.genero;
     var orden = req.query.columna_orden; // TITULO PUNTUACION O MAS NUEVA
     var tipoOrden = req.query.tipo_orden; // ASC DESC
     if(anio === undefined && titulo === undefined && genero === undefined){
-        sql = 'select * from pelicula' + " limit "+ pagina +","+ cantidad;
-        total = 'select count(*) as total from pelicula';
+             sql = 'select * from pelicula' + " limit "+ pagina +","+ cantidad;
+             total = 'select count(*) as total from pelicula';
     }
     if(anio != undefined && titulo != undefined){
 
-        sql = 'select * from pelicula where anio = '+anio+' and titulo like "'+titulo+'%'+'" order by '+ orden + ' ' + tipoOrden + " limit "+ pagina +","+ cantidad;
-        total = 'select count(*) as total from pelicula where anio = '+anio+' and titulo like "'+titulo+'%'+'" order by '+ orden + ' ' + tipoOrden;
+            sql = 'select * from pelicula where anio = '+anio+' and titulo like "'+titulo+'%'+'" order by '+ orden + ' ' + tipoOrden + " limit "+ pagina +","+ cantidad;
+            total = 'select count(*) as total from pelicula where anio = '+anio+' and titulo like "'+titulo+'%'+'" order by '+ orden + ' ' + tipoOrden;
     }
     if(anio != undefined && genero != undefined){
 
-        sql = 'select * from pelicula where anio = '+anio+' and genero_id = '+genero + ' order by '+ orden + ' ' + tipoOrden + " limit "+ pagina +","+ cantidad;
-        total = 'select count(*) as total from pelicula where anio = '+anio+' and genero_id = '+genero + ' order by '+ orden + ' ' + tipoOrden;
+            sql = 'select * from pelicula where anio = '+anio+' and genero_id = '+genero + ' order by '+ orden + ' ' + tipoOrden + " limit "+ pagina +","+ cantidad;
+            total = 'select count(*) as total from pelicula where anio = '+anio+' and genero_id = '+genero + ' order by '+ orden + ' ' + tipoOrden;
     }
     if(genero != undefined && titulo != undefined){
 
-        sql = 'select * from pelicula where genero_id = '+genero+' and titulo like "'+titulo+'%'+'"' + ' order by '+ orden + ' ' + tipoOrden + " limit "+ pagina +","+ cantidad;
-        total = 'select count(*) as total from pelicula where genero_id = '+genero+' and titulo like "'+titulo+'%'+'"' + ' order by '+ orden + ' ' + tipoOrden;
+            sql = 'select * from pelicula where genero_id = '+genero+' and titulo like "'+titulo+'%'+'"' + ' order by '+ orden + ' ' + tipoOrden + " limit "+ pagina +","+ cantidad;
+            total = 'select count(*) as total from pelicula where genero_id = '+genero+' and titulo like "'+titulo+'%'+'"' + ' order by '+ orden + ' ' + tipoOrden;
 
     }
     if(genero === undefined && titulo === undefined && anio != undefined){
-                sql = 'select * from pelicula where anio = '+anio + ' order by '+ orden + ' ' + tipoOrden + " limit "+ pagina +","+ cantidad;
-                total = 'select count(*) as total from pelicula where anio = '+anio + ' order by '+ orden + ' ' + tipoOrden;
+            sql = 'select * from pelicula where anio = '+anio + ' order by '+ orden + ' ' + tipoOrden + " limit "+ pagina +","+ cantidad;
+            total = 'select count(*) as total from pelicula where anio = '+anio + ' order by '+ orden + ' ' + tipoOrden;
 
     }
     if(genero != undefined && titulo === undefined && anio === undefined){
-                sql = 'select * from pelicula where genero_id = '+genero + ' order by '+ orden + ' ' + tipoOrden + " limit "+ pagina +","+ cantidad;
-                total = 'select count(*) as total from pelicula where genero_id = '+genero + ' order by '+ orden + ' ' + tipoOrden;
+            sql = 'select * from pelicula where genero_id = '+genero + ' order by '+ orden + ' ' + tipoOrden + " limit "+ pagina +","+ cantidad;
+            total = 'select count(*) as total from pelicula where genero_id = '+genero + ' order by '+ orden + ' ' + tipoOrden;
     }
     if(genero === undefined && titulo != undefined && anio === undefined){
-                sql = 'select * from pelicula where titulo like "'+titulo+'%'+'"' + ' order by '+ orden + ' ' + tipoOrden + " limit "+ pagina +","+ cantidad;
-                total = 'select count(*) as total from pelicula where titulo like "'+titulo+'%'+'"' + ' order by '+ orden + ' ' + tipoOrden;
+            sql = 'select * from pelicula where titulo like "'+titulo+'%'+'"' + ' order by '+ orden + ' ' + tipoOrden + " limit "+ pagina +","+ cantidad;
+            total = 'select count(*) as total from pelicula where titulo like "'+titulo+'%'+'"' + ' order by '+ orden + ' ' + tipoOrden;
     }
     console.log(sql);
+    
     con.query(sql,function(error,resultado,fields){
             if(error){
                 console.log('Hubo un error en la consulta', error.message);
@@ -54,9 +55,7 @@ function devolverTodasLasPeliculas(req,res){
             }
             con.query(total,function(error,resultadoTotal){
 
-           
             var respuesta = {
-
                     'peliculas': resultado,
                     'total': resultadoTotal[0].total,
             }
@@ -82,8 +81,35 @@ function devolverGeneros(req,res){
         res.send(JSON.stringify(respuesta));
     })
 }
+
+function devolverInfoPelicula(req,res){
+var id = req.params.id;
+var actores = 'select actor.nombre from pelicula inner join actor_pelicula on pelicula.id = actor_pelicula.pelicula_id join actor on actor.id = actor_pelicula.actor_id where pelicula.id = '+ id;
+var pelicula = 'select titulo,trama,puntuacion,duracion,fecha_lanzamiento,director,anio,genero.nombre from pelicula join genero on pelicula.genero_id = genero.id where pelicula.id = '+id;
+con.query(pelicula,function(error,resultado,fields){
+    if(error){
+        console.log('Ha ocurrido un error, el id es invalido');
+        return res.status(404).send('hubo un error en la consulta');
+    }
+
+    con.query(actores,function(err,actores){
+
+            var respuesta = {
+
+                    'pelicula': resultado[0],
+                    'actores': actores,
+                    'genero': resultado[0],
+
+            }
+            res.send(JSON.stringify(respuesta));
+        })
+
+
+});
+}
 module.exports = {
 
     mostrarTodasLasPeliculas: devolverTodasLasPeliculas,
     mostrarTodosLosGeneros: devolverGeneros,
+    mostrarPeliculaporid: devolverInfoPelicula,
 }
